bdf829d4797baaa78ec2484070d397600e74a8d4
sistemascaff <sistemas@froebel.edu.bo>
2025-06-12 09:03:17 -0400

    Hotfix

--- Model/Session.cs
+++ Model/Session.cs
@@ -13,7 +13,7 @@ namespace sisgesoriadao.Model
     {
         //Cadena de conexión requerida para llamar a la base de datos.
         public static string CadenaConexionBdD { get; set; } = "server=162.144.4.18;database=planetc5_bdventacelular;uid=planetc5_root;pwd=thejockeydeath1;port=3306";
-        public static string VersionApp { get; set; } = "v. 1.8 pt.1";
+        public static string VersionApp { get; set; } = "v. 1.8.1";
         //Atributo indispensable para manejar la totalidad del sistema.
         public static byte IdUsuario { get; set; }
         //Atributo de referencia para dar a conocer al usuario que ha iniciado sesión correctamente.
